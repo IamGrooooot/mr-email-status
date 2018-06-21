9fba373f5ea0f7fa135a840ca4b24bb46ec26fa5
Galchinsky Vadim (Work) <rorshah.009@gmail.com>
2018-06-21 17:29:15 +0300

    for old branch router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { DropdownDirective } from './shared/directives/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './system/navbar/navbar.component';
 
 @NgModule({
   declarations: [
